522b49384968836c0e1750c183fdf80195d23557
gedehariyogananda <gedehariyoga@gmail.com>
2024-09-27 19:17:17 +0700

    feat: interaction schema follow user komunitas and personal

---
+++ db/migrations/20240927121511_create_interaction_schema.sql
@@ -0,0 +1,19 @@
+-- migrate:up
+CREATE SCHEMA IF NOT EXISTS "interaction";
+
+-- create table interaction.follow user personal and user instansi
+DROP TABLE IF EXISTS "interaction"."follow" CASCADE;
+CREATE TABLE "interaction"."follow"(
+  "id"            uuid          DEFAULT uuid_generate_v4() ,
+  "follower_id" uuid NOT NULL,
+  "following_id" uuid NOT NULL,
+  "status_follow" BOOLEAN DEFAULT FALSE NOT NULL,
+  "created_at"        TIMESTAMP   		NOT NULL  DEFAULT CURRENT_TIMESTAMP ,
+  "updated_at"        TIMESTAMP   		NOT NULL  DEFAULT CURRENT_TIMESTAMP ,
+  PRIMARY KEY ("id"),
+  FOREIGN KEY ("follower_id") REFERENCES "user"."account"("id")  ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY ("following_id") REFERENCES "user"."account"("id")  ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- migrate:down
+
